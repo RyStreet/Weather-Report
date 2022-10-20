ccb9cd7d1787b84706839b542692ef8bb1edc4be
Ryan Street <ryanstreet122@gmail.com>
2022-10-20 13:19:17 -0400

    added background color classes

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
+//Display current time on page header//
+
+const currentDay = document.getElementById("currentDay"); //gets empty <p> tag //
+
+var now = document.createTextNode(dayjs().format("dddd, MMMM DD")); //creates text node that includes dayjs format for current date//
+
+currentDay.appendChild(now) //appends currentDay text to empty <p> tag//
+
 //Open Weather API call 
 //my API Key = 2b5269240b8365eece7f67a1d5fe64d7
 

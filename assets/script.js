f8c3f4fcf32dc0ea15c447fc146486db5cc6cd63
Ryan Street <ryanstreet122@gmail.com>
2022-10-21 12:58:06 -0400

    working on input search functionality JS

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,28 @@ var now = document.createTextNode(dayjs().format("dddd, MMMM DD")); //creates te
 
 currentDay.appendChild(now) //appends currentDay text to empty <p> tag//
 
+//add search functionality
+
+const input = document.querySelector('input');
+input.addEventListener('search', updateCity)
+
+const cityName = document.getElementById('cityName');
+
+
+
+function updateCity(e){
+  cityName.textContent = e.target.value;
+
+  getAPI();
+  console.log(cityName.textContent)
+}
+
 //Open Weather API call 
 //my API Key = 2b5269240b8365eece7f67a1d5fe64d7
 
-function getAPI(){
+function getAPI(e){
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=boston&appid=2b5269240b8365eece7f67a1d5fe64d7'
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=2b5269240b8365eece7f67a1d5fe64d7'
 fetch(requestUrl)
 .then(function (response) {
   return response.json();  
@@ -21,6 +37,6 @@ fetch(requestUrl)
 })
 };
 
-getAPI();
+
 // console.log(getAPI);
 // console.log(response.json())

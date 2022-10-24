858af6505445ea122c13156049e84cfa276add4e
Ryan Street <ryanstreet122@gmail.com>
2022-10-24 12:24:45 -0400

    dynamic icons and background added in JS

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ const currentDay = document.getElementById("currentDay"); //gets empty <p> tag /
 
 var now = document.createTextNode(dayjs().format("dddd, MMMM DD")); //creates text node that includes dayjs format for current date//
 
+
+
 currentDay.appendChild(now) //appends currentDay text to empty <p> tag//
 
 //add search functionality
@@ -20,10 +22,10 @@ var currentTemp = document.getElementById("currentTemp")
 var currentWind = document.getElementById("currentWind")
 var currentHumidity = document.getElementById("currentHumidity")
 var weatherIconCurrent = document.getElementById("weatherIconCurrent")
-
+var body = document.getElementById('fullPage')
 //updates cityName div with value inputted into search bar, calls API function
 function updateCity(e){
-  cityName.textContent = e.target.value;
+  
   getAPI();
 }
 
@@ -44,31 +46,44 @@ function getAPI(e){
   .then(function (data){
       console.log(data)
 
+      //displays city name from data in cityName div
+      console.log(data.city.name)
+      var cityNameStyled = data.city.name
+      cityName.textContent = cityNameStyled
+
+      //displays current temp data
       var tempNow = data.list[0].main.feels_like
-      console.log(tempNow)
+      console.log(tempNow + " f")
       currentTemp.textContent = "Temp: " + tempNow + " F"
       
-
-      
-
+      //displays current wind data
       var windNow = data.list[0].wind.speed
-      console.log(windNow)
+      console.log(windNow + " m/s")
       currentWind.textContent = "Wind: " + windNow + " m/s"
       
-
+      //displays current humidity data
       var humidNow = data.list[0].main.humidity
-      console.log(humidNow)
+      console.log(humidNow + " %")
       currentHumidity.textContent = "Humidity: " + humidNow + "%"
 
-
-      var rainNow = data.list[0].rain
-      console.log(data.list[0].rain3h)
-      if(rainNow <= .2){
-        weatherIconCurrent.removeClass = "fa-sun";
+      var timeNow = data.city.sunset
+      console.log(timeNow + " sunset time")
+      //dynamically updates weather icon to display rain if rain data is > 40%
+      var rainNow = data.list[0].rain['3h'];
+      console.log(rainNow + " % chance of rain")
+      if(rainNow > .40){
+        weatherIconCurrent.classList.remove('fa-sun')
+        weatherIconCurrent.classList.add("fa-cloud-rain");
+        body.classList.remove("daySunny")
+        body.classList.add("dayCloudy")
       }
-      
-      
-      
+      else{
+        body.classList.remove("dayCloudy")
+        body.classList.add("daySunny")
+        weatherIconCurrent.classList.add('fa-sun');
+        
+      }
+
   })
 };
 
